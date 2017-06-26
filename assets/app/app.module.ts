831a2c09fa877ea88ff2f52938156ca89b539b7f
onalv <omarnavaalvarez@gmail.com>
2017-06-25 22:09:05 -0500

    error handlers added

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -15,6 +15,8 @@ import {LogoutComponent} from "./auth/logout.component";
 import {SigninComponent} from "./auth/signin.component";
 import {SignupComponent} from "./auth/signup.component";
 import {AuthService} from "./auth/auth.service";
+import {ErrorComponent} from "./errors/error.component";
+import {ErrorService} from "./errors/error.service";
 
 @NgModule({
     declarations: [
@@ -27,7 +29,8 @@ import {AuthService} from "./auth/auth.service";
         HeaderComponent,
         LogoutComponent,
         SigninComponent,
-        SignupComponent
+        SignupComponent,
+        ErrorComponent
     ],
     imports: [
         BrowserModule,
@@ -35,7 +38,7 @@ import {AuthService} from "./auth/auth.service";
         routing,
         ReactiveFormsModule,
         HttpModule],
-    providers: [AuthService],
+    providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
